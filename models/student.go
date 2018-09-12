5ac2a339f8a4e850a05ffcb9defbe738462659c3
deathsy <kittipong.sri@mail.kmutt.ac.th>
2018-09-12 17:20:00 +0700

    [ADD] student model structure

--- models/student.go
+++ models/student.go
@@ -1,1 +1,11 @@
 package models
+
+import "github.com/globalsign/mgo/bson"
+
+type Student struct {
+	Id        bson.ObjectId `bson:"id" json:"id"`
+	StudentId string        `bson:"studentId" json:"studentId"`
+	Name      string        `bson:"name" json:"name"`
+	CreatedAt string        `bson:"createdAt" json:"createdAt"`
+	UpdatedAt string        `bson:"updatedAt" json:"updatedAt"`
+}
