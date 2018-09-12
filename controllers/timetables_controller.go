7b7a0a29939aa6bc3f3fe5d2c9644711096559ca
deathsy <kittipong.sri@mail.kmutt.ac.th>
2018-09-12 19:49:15 +0700

    [ADD] timetable controller

--- controllers/timetables_controller.go
+++ controllers/timetables_controller.go
@@ -1,8 +1,17 @@
 package controllers
 
 import (
+	"github.com/deathsy/tmga-ga/config"
+	"github.com/deathsy/tmga-ga/repositories"
 	"net/http"
 )
+
+var timetableRepo repositories.TimetableRepository
+
+func init() {
+	session := config.Connect()
+	timetableRepo = repositories.TimetableRepository{session, "Timetable"}
+}
 
 func GetAllTimetables(w http.ResponseWriter, r *http.Request) {
 
