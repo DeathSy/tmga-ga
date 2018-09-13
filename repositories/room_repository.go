1b2cd3d4afb83a683d8b415540ece15339a23be7
deathsy <kittipong.sri@mail.kmutt.ac.th>
2018-09-13 13:50:51 +0700

    [ADD] room repository

---
+++ repositories/room_repository.go
@@ -0,0 +1,28 @@
+package repositories
+
+import (
+	"github.com/deathsy/tmga-ga/models"
+	"github.com/globalsign/mgo"
+	"github.com/globalsign/mgo/bson"
+)
+
+type RoomRepository struct {
+	DB         *mgo.Database
+	Collection string
+}
+
+func (r *RoomRepository) FindAll() ([]models.Room, error) {
+	var rooms []models.Room
+
+	err := r.DB.C(r.Collection).Find(bson.M{}).All(&rooms)
+
+	return rooms, err
+}
+
+func (r *RoomRepository) Find(id string) (models.Room, error) {
+	var room models.Room
+
+	err := r.DB.C(r.Collection).FindId(bson.ObjectIdHex(id)).One(&room)
+
+	return room, err
+}
