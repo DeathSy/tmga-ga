302811a725661b14b0ba8b5b81083a94a21a2cfa
deathsy <kittipong.sri@mail.kmutt.ac.th>
2018-09-16 16:44:09 +0700

    [ADD] default response to timetable creation

--- controllers/timetables_controller.go
+++ controllers/timetables_controller.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"github.com/deathsy/tmga-ga/config"
 	"github.com/deathsy/tmga-ga/repositories"
+	"github.com/deathsy/tmga-ga/services"
 	"net/http"
 )
 
@@ -22,7 +23,10 @@ func GetTimetable(w http.ResponseWriter, r *http.Request) {
 }
 
 func CreateTimeTable(w http.ResponseWriter, r *http.Request) {
-
+	var response interface{}
+	ga := services.Genetic{}
+	ga.PreparingData()
+	services.RespondWithJson(w, http.StatusOK, response)
 }
 
 func DeleteTimetable(w http.ResponseWriter, r *http.Request) {
