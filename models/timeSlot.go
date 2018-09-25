ecbcdbd7182ca07f670d96484e7b12ace860a0b6
deathsy <kittipong.sri@mail.kmutt.ac.th>
2018-09-25 14:54:30 +0700

    [EDIT] timeslot model attr type

--- models/timeSlot.go
+++ models/timeSlot.go
@@ -1,11 +1,14 @@
 package models
 
-import "github.com/globalsign/mgo/bson"
+import (
+	"github.com/globalsign/mgo/bson"
+	"time"
+)
 
 type TimeSlot struct {
 	Id        bson.ObjectId `bson:"_id" json:"_id"`
 	Start     string        `bson:"start" json:"start"`
 	End       string        `bson:"end" json:"end"`
-	CreatedAt string        `bson:"createdAt" json:"createdAt"`
-	UpdatedAt string        `bson"updatedAt" json:"updatedAt"`
+	CreatedAt time.Time     `bson:"createdAt" json:"createdAt"`
+	UpdatedAt time.Time     `bson:"updatedAt" json:"updatedAt"`
 }
