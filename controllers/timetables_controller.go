0c367c8a1a26a8b58fefb2072d8a6c07d9b52ddb
deathsy <kittipong.sri@mail.kmutt.ac.th>
2018-09-28 06:12:31 +0700

    [EDIT] controller to run a command

--- controllers/timetables_controller.go
+++ controllers/timetables_controller.go
@@ -1,34 +1,19 @@
 package controllers
 
 import (
-	"github.com/deathsy/tmga-ga/config"
-	"github.com/deathsy/tmga-ga/repositories"
 	"github.com/deathsy/tmga-ga/services"
 	"net/http"
+	"os/exec"
 )
 
-var timetableRepo repositories.TimetableRepository
-
-func init() {
-	session := config.Connect()
-	timetableRepo = repositories.TimetableRepository{session, "Timetable"}
-}
-
-func GetAllTimetables(w http.ResponseWriter, r *http.Request) {
-
-}
-
-func GetTimetable(w http.ResponseWriter, r *http.Request) {
-
-}
-
 func CreateTimeTable(w http.ResponseWriter, r *http.Request) {
-	ga := services.Genetic{InitialGeneration: 100}
-	chromosome := ga.Start()
 
-	services.RespondWithJson(w, http.StatusOK, chromosome)
-}
+	cmd := exec.Command("genetic")
+	cmd.Start()
 
-func DeleteTimetable(w http.ResponseWriter, r *http.Request) {
+	response := struct {
+		Message string
+	}{Message: "success"}
 
+	services.RespondWithJson(w, http.StatusOK, response)
 }
